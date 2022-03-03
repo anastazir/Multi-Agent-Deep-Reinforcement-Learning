d0913bf8724e5d9214d8af9c8c808ede435a3788
Anas Tazir <atazir2018@gmail.com>
2022-03-03 11:39:31 +0530

    update confi.py

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 N_AGENTS          = 1
 GRID_SIZE         = 5
+POSSIBLE_ACTIONS  = ['U', 'D', 'L', 'R', 'S']
+POSSIBLE_ACTIONS_NUM = [0, 1 ,2, 3, 4]
 
 PLAYER_POS        = [(0,2),(1,5),(0,6),(0,3)]
 ENEMY_POS         = [(4,4),(3,2),(6,6),(6,1)]
 
 BATCH_SIZE        = 256
 REPLAY_MEMORY_LEN = 10000
+EPOCHS            = 1
 
-ACTION_SIZE       = 5
+ACTION_SIZE       = len(POSSIBLE_ACTIONS)
 
 LEARNING_RATE     = 0.8   # 5e-05
 GAMMA             = 0.95  # 0.6
@@ -15,13 +18,14 @@ DECAY_RATE        = 0.05
 MAX_EPSILON       = 1.0
 MIN_EPSILON       = 0.01
 
-EPOCHS            = 1
-REPLAY_STEPS      = 8
-TIME_STEPS        = 99
-EPISODES          = 200
-NUM_NODES         = 256
+REPLAY_STEPS      = 5
+TIME_STEPS        = 51
+EPISODES          = 400
 
 POSITIVE_REWARD   = 10
-PROXIMITY_REWARD  = 1
+PROXIMITY_REWARD  = 0
 NEGATIVE_REWARD   = -1
-NO_REWARD         = 0
+PENALTY_REWARD    = -2
+
+HIDDEN_LAYER_01   = 256
+HIDDEN_LAYER_02   = 128
