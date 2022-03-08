cab78e006736d741705c3551ec1024271f6bff88
Anas Tazir <atazir2018@gmail.com>
2022-03-08 13:48:05 +0530

    update file

--- multi_agent.py
+++ multi_agent.py
@@ -5,18 +5,19 @@ from    agent           import Agent
 from    enviroment      import Enviroment
 from    IPython.display import clear_output
 from    matplotlib      import pyplot as plt
+import  random
 
 grid_size = GRID_SIZE
 num_col = grid_size
 
-possibleActions = ['U', 'D', 'L', 'R', 'S']
+possibleActions = POSSIBLE_ACTIONS
 
 action_space_dict = {
-    "U" : 1,
-    "D" : 2,
-    "L" : 3,
-    "R" : 4,
-    "S" : 0
+    "U" : 0,
+    "D" : 1,
+    "L" : 2,
+    "R" : 3,
+    "S" : 4
 }
 
 n_agents          = N_AGENTS
@@ -24,81 +25,17 @@ allplayerpos      = PLAYER_POS[: n_agents]
 enemy_list_pos    = ENEMY_POS[: n_agents]
 batch_size        = BATCH_SIZE
 replay_memory_len = REPLAY_MEMORY_LEN
+type = "sticky"
 
 def decode_state(state_num):
     return int(state_num/num_col), state_num%num_col
 
 def state_encode(row,col):
-    return row*num_col + col 
+    return row*num_col + col
 
-def run():
-    total_step = 0
-    rewards_list = []
-    timesteps_list = []
-
-    for episode in range(EPISODES):
-        print("Episode number: " + episode)
-
-        reward_all = 0
-        time_step = 0
-        i = 0
-
-        for agent in all_agents:
-            agent.terminal = False
-        
-        states = env.reset()
-        states = np.concatenate((states,enemy_states)).ravel()
-
-        for agent in all_agents:
-            agent.set_pos(allplayerpos[i])
-            i = i + 1
-
-        done = [False for _ in range(n_agents)]
-
-        while not any(done):
-
-            env.render(clear=True)
-            actions = []
-
-            for agent in all_agents:
-                actions.append(agent.act(states, possibleActions))
-
-            next_states, rewards, done = env.step(actions)
-            for agent in all_agents:
-
-                next_states = np.array(next_states)
-                next_states = next_states.ravel()
-
-                agent.set_pos(decode_state(next_states[agent.index]))
-                agent.store(np.concatenate((states,enemy_states)).ravel(), action_space_dict[actions[agent.index]], \
-                rewards[agent.index], np.concatenate((next_states,enemy_states)).ravel(), done[agent.index])
-
-                if done[agent.index] == True:
-                    agent.terminal = True
-                    print("agent reached landmark")
-
-            total_step += 1
-            time_step += 1
-            states = next_states
-            reward_all += sum(rewards)
-
-            for agent in all_agents:
-                if agent.terminal:
-                    agent.alighn_target_model()
-                if time_step % REPLAY_STEPS == 0:
-                    agent.retrain()
-
-        for agent in all_agents:
-            agent.decay_epsilon(episode)
-
-        rewards_list.append(reward_all)
-        timesteps_list.append(time_step)
-
-all_agents= []
-
-for index in range(n_agents):
-
-    all_agents.append(Agent(index, allplayerpos[index], batch_size, replay_memory_len))
+all_agents = []
+for i in range(0, N_AGENTS):
+    all_agents.append(Agent(i, allplayerpos[i], type = type))
 
 
 initial_states = []
@@ -109,4 +46,89 @@ enemy_states = []
 for enemy_pos in enemy_list_pos:
     enemy_states.append(state_encode(enemy_pos[0], enemy_pos[1]))
 
-env = Enviroment(grid_size = grid_size, initial_states = initial_states, enemy_states = enemy_states, n_agents = n_agents)
+env = Enviroment(initial_states = initial_states, enemy_states = enemy_states, type = type)
+
+def run():
+    total_step = 0
+    rewards_list = []
+    timesteps_list = []
+    total_steps = 1
+    for episode in range(1, EPISODES):
+        print("Episode number: ", episode)
+
+        reward_all = 0
+        time_step = 1
+        for agent in all_agents:
+            agent.terminal = False
+        
+        [states, enemy_states] = env.reset()
+        print("player states: ", states)
+        for agent in all_agents:
+            agent.set_pos(allplayerpos[agent.index])
+
+        done = [False for _ in range(n_agents)]
+
+        state_arr = np.zeros(STATE_SIZE)
+        state_arr[states] = 1
+        state_arr[enemy_states] = 2
+        # state_arr[np.concatenate((states,enemy_states))] = 1
+        old_states = np.reshape(state_arr, [1, STATE_SIZE])
+        # print("old states are", old_states)
+
+        while not all(done):
+
+            # env.render(clear=True)
+            actions = []
+            for agent in all_agents:
+
+                actions.append(agent.act(old_states, possibleActions))
+
+            next_states, rewards, done = env.step(actions)
+
+            for agent in all_agents:
+                agent.set_pos(decode_state(next_states[agent.index]))
+
+            nstate_arr = np.zeros(STATE_SIZE)
+            nstate_arr[next_states] = 1
+            nstate_arr[enemy_states] = 2
+            # nstate_arr[np.concatenate((next_states,enemy_states))] = 1
+            new_states = np.reshape(nstate_arr, [1, STATE_SIZE])
+            # print("new states are", new_states)
+            for agent in all_agents:
+                agent.store(new_states, rewards[agent.index], \
+                done[agent.index], old_states, action_space_dict[actions[agent.index]])
+
+                if done[agent.index] == True:
+                    agent.terminal = True
+                    print("agent reached landmark--------------------------------")
+
+            print("actions", actions)
+
+            for agent in all_agents:
+                print("agent epsilon ", agent.epsilon, "agent memory len",\
+                    len(agent.expirience_replay), "steps ", time_step,\
+                    "reward", rewards[0], "next state ", next_states[0], "agent position ", agent.return_coordinates())
+
+            if time_step >= TIME_STEPS:
+                print("max steps reached")
+                break
+
+            old_states = new_states
+
+            total_step += 1
+            time_step += 1
+            total_steps+1
+            reward_all += sum(rewards)
+
+            if all(done):
+                print("-----------------------------------all agents reached landmark--------------------------------")
+                for agent in all_agents:
+                    agent.save_model()
+        print("total rewards", reward_all)
+        for agent in all_agents:
+            agent.retrain(episode)
+
+        rewards_list.append(reward_all)
+        timesteps_list.append(time_step)
+
+run()
