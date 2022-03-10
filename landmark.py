5ad3e8b4a14c2ab06dafd750dc6bc50a53bf6a9a
Anas Tazir <atazir2018@gmail.com>
2022-03-10 23:39:58 +0530

    update landmark.py

--- landmark.py
+++ landmark.py
@@ -1,7 +1,8 @@
 from config import *
 
 class Landmark:
-    def __init__(self, state, is_captured=False):
+    def __init__(self, index, state, is_captured=False):
+        self.index = index
         self.state = state
         self.is_captured = is_captured
         self.x, self.y = self.decode_state()
@@ -10,7 +11,8 @@ class Landmark:
         return int(self.state / GRID_SIZE), self.state % GRID_SIZE
 
     def captured(self):
+        print("landmark captured", self.index)
         self.is_captured = True
 
     def reset(self):
-        self.is_captured = True
+        self.is_captured = False
